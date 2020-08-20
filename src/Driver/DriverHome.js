6aee3f9d2b1a4ffb74329713c050518a281bac6b
Juan Villate <jmvillatei@consorcioqutubminar.com>
2020-08-20 05:17:44 -0500

    ignore node_modules

---
+++ src/Driver/DriverHome.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import './DriverHome.css';
+
+
+class DriverHome extends React.Component{
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-dark bg-dark">
+                    CADAN WHEELS
+                </nav>
+                
+            </div>
+        );
+    }
+}
+
+
+export default DriverHome;
