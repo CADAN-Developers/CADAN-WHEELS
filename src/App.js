1b058025b48995120e6f7a8cd96a9f822629ba3d
Andrés González <andres.gonzalez-ca@mail.escuelaing.edu.co>
2020-10-29 15:28:09 -0500

    Agregar vehiculo y consultar vehiculos usuario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import moment from "moment";
 import './App.css';
-import DriverHome from './Driver/home/DriverHome';
+import DriverHome from './driver/home/DriverHome';
 import PasajeroHome from './pasajero/home/PasajeroHome'
 import UpdatePasajero from './pasajero/home/UpdatePasajero'
 import RecordsPasajero from './pasajero/home/RecordsPasajero'
-import FormVehiculo from './Driver/registroVehiculo/FormVehiculo'
+import FormVehiculo from './driver/registroVehiculo/FormVehiculo'
 import Login from './components/Login'
 import RegistroUsuario from "./pasajero/registroUsuario/RegistroUsuario";
+import axios from 'axios';
 
 
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
@@ -18,14 +19,20 @@ class App extends Component {
 
         super(props);
         this.state = {
-            isLog : false
+            isLog : false,
+            items : []
+            
 
         }
         this.handleClick = this.handleClick.bind(this)
         
+        
      }
 
     render() {
+        
+        
+          /*          
         const items = [{
             marca: "Renault ",
             matricula: moment(new Date(156464645646)),
@@ -33,10 +40,13 @@ class App extends Component {
             capacidad: "5",
             soat: moment(new Date(156464645646))
           }]
+          */
+
           
+          console.log("JEG "+this.state.items);
           const VehiculosView = () => (
             <div>
-              <FormVehiculo vehicList={items} /> 
+              <FormVehiculo vehicList={this.state.items} /> 
             </div>
         );
 
@@ -79,7 +89,25 @@ class App extends Component {
             </div>
         );
     }
+    async componentDidMount(){
+        const url = "https://cadanback.herokuapp.com/getVehiculos?conductor=daniel@mail.com"
+        //axios.get(url).then(res => res.json()).then(res => console.log(res));
+        //fetch(url).then(res => console.log(res)).catch(error => console.log("JOHANN ES RE GURRERO "+ error));
+        var list=[];
+        await fetch(url).then(res => res.json())
+                    .then((result) => {
+                            result.forEach(element => { list.push(element)
+                                
+                            });
+                                
+                          
+                        
+                    });
+        this.setState({
+            items: list
+        });
 
+    }
 
     handleClick(e) {
         console.log("regresa a APP ")
